1b3daf4aa68e38fa0a73237a4a2418f6a55d97b1
VasilyKuzu <vasilykuzn03@gmail.com>
2025-09-04 00:08:58 +0300

    Добавил миддлвар обработки ошибок

---
+++ API/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+
+namespace CRM.API
+{
+    public sealed class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = new
+                {
+                    status = 500,
+                    message = "Произошла ошибка на сервере",
+                    detail = ex.Message
+                };
+
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+
+                var json = JsonSerializer.Serialize(errorResponse);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
